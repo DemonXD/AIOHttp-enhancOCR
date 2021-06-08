1a869cf1719c0fb0808d6a3d8a464a3d2b6f4687
miles xu <kanonxmm@163.com>
2021-06-08 17:38:51 +0800

    add crnn link

---
+++ torch/crnn/main.py
@@ -0,0 +1,11 @@
+#################################
+# Date: 2021/06/08
+# Author: Miles Xu
+# Email: kanonxmm@163.com
+# Desc.: 参考
+# https://github.com/Belval/CRNN.git
+#################################
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    pass
